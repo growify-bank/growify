699d873201940bd8840303d7a8aac2af92c40d5d
r. ávila <avila.dev@outlook.com>
2024-11-22 22:57:43 -0300

    Initial commit

---
+++ src/main/java/org/growify/bank/GrowifyApplication.java
@@ -0,0 +1,11 @@
+package org.growify.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowifyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GrowifyApplication.class, args);
+    }
+}
---
+++ src/test/java/org/growify/bank/GrowifyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.growify.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
