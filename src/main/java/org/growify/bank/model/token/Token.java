8c747a40c9e4f8f662cccc106625cf93ba0c6d88
r. ávila <avila.dev@outlook.com>
2024-11-23 23:59:05 -0300

    feat: add data jpa's entity annotations

--- src/main/java/org/growify/bank/model/token/Token.java
+++ src/main/java/org/growify/bank/model/token/Token.java
@@ -1,4 +1,18 @@
 package org.growify.bank.model.token;
 
-public class Token {
+import jakarta.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Table(schema = "user_tokens")
+@Entity public class Token {
+    @Getter
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Id private UUID id;
 }
