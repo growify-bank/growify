d969d916982c147889a005434a8025b719a0cfb0
Rafael Silva <ainbot6@gmail.com>
2024-11-28 21:17:07 -0300

    feat: create TokenRepository (#7)

--- src/main/java/org/growify/bank/repository/TokenRepository.java
+++ src/main/java/org/growify/bank/repository/TokenRepository.java
@@ -4,6 +4,9 @@ import org.growify.bank.model.token.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TokenRepository extends JpaRepository<Token, String> {
+    List<Token> findAllValidTokenByUser(String id);
 }
