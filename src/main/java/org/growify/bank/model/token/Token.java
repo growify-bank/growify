32b740f60b665140bfe08230f72a1753aeafee89
Rafael Silva <ainbot6@gmail.com>
2024-11-28 22:32:36 -0300

    feat: add token settings

--- src/main/java/org/growify/bank/model/token/Token.java
+++ src/main/java/org/growify/bank/model/token/Token.java
@@ -1,18 +1,25 @@
 package org.growify.bank.model.token;
 
-import jakarta.persistence.Table;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
-import lombok.Getter;
-
-import java.util.UUID;
+import jakarta.persistence.*;
+import lombok.*;
+import org.growify.bank.model.user.User;
 
 @Table(schema = "user_tokens")
-@Entity public class Token {
-    @Getter
+@Data
+@Getter
+@Setter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Token {
+    @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    @Id private UUID id;
+    private String id;
+    private String tokenValue;
+    private TokenType tokenType;
+    private boolean tokenRevoked;
+    private boolean tokenExpired;
+    private User user;
 }
