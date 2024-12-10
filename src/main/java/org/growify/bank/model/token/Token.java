fb70d1a072262211f85f9d752bf790e3dbb75f5a
Carlos Dantas <carlosdev54@gmail.com>
2024-12-09 21:56:38 -0400

    feat#-20: implement User and Token entities with JPA annotations

--- src/main/java/org/growify/bank/model/token/Token.java
+++ src/main/java/org/growify/bank/model/token/Token.java
@@ -1,18 +1,45 @@
 package org.growify.bank.model.token;
 
-import jakarta.persistence.Table;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
+import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
+
+import org.growify.bank.model.user.User;
 
 import java.util.UUID;
 
+@Entity
 @Table(schema = "user_tokens")
-@Entity public class Token {
+public class Token {
+
     @Getter
+    @Setter
     @GeneratedValue(strategy = GenerationType.UUID)
-    @Id private UUID id;
+    @Id
+    private UUID id;
+
+    @Getter
+    @Setter
+    @Column(nullable = false, unique = true)
+    private String tokenValue;
+
+    @Getter
+    @Setter
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TokenType tokenType;
+
+    @Getter
+    @Setter
+    @Column(nullable = false)
+    private boolean tokenRevoked;
+
+    @Getter
+    @Setter
+    @Column(nullable = false)
+    private boolean tokenExpired;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }
