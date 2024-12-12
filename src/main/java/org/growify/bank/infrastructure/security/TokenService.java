cb7a7bbe17526bfa2323dcb61a03b1194fc9859e
Rafael Silva <ainbot6@gmail.com>
2024-12-11 21:40:26 -0300

    feat: add package security (#8)

--- src/main/java/org/growify/bank/infrastructure/security/TokenService.java
+++ src/main/java/org/growify/bank/infrastructure/security/TokenService.java
@@ -1,7 +1,25 @@
 package org.growify.bank.infrastructure.security;
 
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import org.springframework.stereotype.Service;
 
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
 @Service
+@RequiredArgsConstructor
 public class TokenService {
+
+    @Value("${security.token.private-key}")
+    private RSAPrivateKey privateKey;
+
+    @Value("${security.token.public-key}")
+    private RSAPublicKey publicKey;
+
+    @Value("${security.token.expiration-token}")
+    private Integer expirationToken;
+
+    @Value("${security.token.expiration-refresh-token}")
+    private Integer expirationRefreshToken;
 }
