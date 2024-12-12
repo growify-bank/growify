d77c0de3aba10b08d074ee69c95a5a048802ef97
Rafael Silva <ainbot6@gmail.com>
2024-12-11 21:22:42 -0300

    fix: resolving merge conflicts

--- src/main/java/org/growify/bank/model/token/Token.java
+++ src/main/java/org/growify/bank/model/token/Token.java
@@ -1,53 +1,12 @@
 package org.growify.bank.model.token;
 
-import jakarta.persistence.*;
-<<<<<<< HEAD
-import lombok.Getter;
-import lombok.Setter;
-
-import org.growify.bank.model.user.User;
-
-import java.util.UUID;
-=======
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
 import org.growify.bank.model.user.User;
->>>>>>> b24df8af8bc7f8894831e40516cbbea2999bf1c1
 
-@Entity
-@Table(schema = "user_tokens")
-<<<<<<< HEAD
-public class Token {
-
-    @Getter
-    @Setter
-    @GeneratedValue(strategy = GenerationType.UUID)
-    @Id
-    private UUID id;
-
-    @Getter
-    @Setter
-    @Column(nullable = false, unique = true)
-    private String tokenValue;
-
-    @Getter
-    @Setter
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    private TokenType tokenType;
-
-    @Getter
-    @Setter
-    @Column(nullable = false)
-    private boolean tokenRevoked;
-
-    @Getter
-    @Setter
-    @Column(nullable = false)
-    private boolean tokenExpired;
-
-    @ManyToOne
-    @JoinColumn(name = "user_id", nullable = false)
-=======
 @Data
 @Getter
 @Setter
@@ -64,6 +23,5 @@ public class Token {
     private TokenType tokenType;
     private boolean tokenRevoked;
     private boolean tokenExpired;
->>>>>>> b24df8af8bc7f8894831e40516cbbea2999bf1c1
     private User user;
 }
--- src/main/java/org/growify/bank/model/user/User.java
+++ src/main/java/org/growify/bank/model/user/User.java
@@ -1,41 +1,5 @@
 package org.growify.bank.model.user;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-
-import org.growify.bank.model.token.Token;
-
-import java.util.List;
-import java.util.UUID;
-
-@Getter
-@Setter
-@Entity
-@Table(schema = "users", name = "users")
-	public class User {
-
-    @GeneratedValue(strategy = GenerationType.UUID)
-    @Id private UUID id;
-
-    @Column(nullable = false, unique = true)
-    private String email;
-
-    @Column(nullable = false, unique = false)
-    private String name;
-
-    @Column(nullable = false, unique = false)
-    private String password;
-
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false, unique = false)
-    private UserRole role;
-
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Token> tokens;
-
-    public User(){
-=======
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
