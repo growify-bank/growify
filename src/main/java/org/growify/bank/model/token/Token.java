2d1d993c5b3b4d3f9bba36ebf25e6cbc7db74b39
Carlos Devnt <carlosdev54@gmail.com>
2024-12-12 21:30:22 -0400

    feat-#20: Implement all user and token entities fields

--- src/main/java/org/growify/bank/model/token/Token.java
+++ src/main/java/org/growify/bank/model/token/Token.java
@@ -1,27 +1,35 @@
 package org.growify.bank.model.token;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.*;
+import jakarta.persistence.*;
+import lombok.Data;  
+
 import org.growify.bank.model.user.User;
 
-@Data
-@Getter
-@Setter
-@Entity
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
+import java.util.UUID;
 
+@Entity
+@Table(schema = "user_tokens")
+@Data
 public class Token {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private String id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
     private String tokenValue;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private TokenType tokenType;
+
+    @Column(nullable = false)
     private boolean tokenRevoked;
+
+    @Column(nullable = false)
     private boolean tokenExpired;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 }
--- src/main/java/org/growify/bank/model/user/User.java
+++ src/main/java/org/growify/bank/model/user/User.java
@@ -1,72 +1,37 @@
 package org.growify.bank.model.user;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.Data;
+
 import org.growify.bank.model.token.Token;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
+import java.util.UUID;
 
-@Table(schema = "users")
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-public class User implements UserDetails {
+@Data
+@Entity
+@Table(schema = "users", name = "users")
+public class User {
 
-    @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private String id;
-    private String name;
+    @Id private UUID id;
+
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
     private String password;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private UserRole role;
-    private transient List<Token> tokens = new ArrayList<>();
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        if (this.role == UserRole.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"));
-        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
-    }
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Token> tokens;
 
-    @Override
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public String getUsername() {
-        return email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return false;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return false;
+    public User() {
     }
 }
