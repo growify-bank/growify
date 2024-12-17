e574c8eb0927cb86fdbdf8fe0c85683c8ea95cb3
Carlos Devnt <carlosdev54@gmail.com>
2024-12-16 22:41:02 -0400

    fix: add single-argument constructor to TokenInvalidException to allow exception instantiation with only a message

--- src/main/java/org/growify/bank/exception/TokenInvalidException.java
+++ src/main/java/org/growify/bank/exception/TokenInvalidException.java
@@ -1,6 +1,10 @@
 package org.growify.bank.exception;
 
-public class TokenInvalidException extends RuntimeException{
+public class TokenInvalidException extends RuntimeException {
+
+    public TokenInvalidException(String message) {
+        super(message);
+    }
 
     public TokenInvalidException(String message, Throwable cause) {
         super(message, cause);
