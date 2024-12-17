4fa71dd6f9b6cb0cfc7c7cf45d588e8d1b33848e
Rafael Silva <ainbot6@gmail.com>
2024-12-16 21:47:10 -0300

    feat: add authentication controller restfull

--- src/main/java/org/growify/bank/controller/AuthController.java
+++ src/main/java/org/growify/bank/controller/AuthController.java
@@ -1,35 +1,54 @@
 package org.growify.bank.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.growify.bank.dto.request.LoginRequestDTO;
-import org.growify.bank.dto.request.RefreshTokenDTO;
+import org.growify.bank.dto.request.RefreshTokenRequestDTO;
 import org.growify.bank.dto.request.RegisterRequestDTO;
 import org.growify.bank.dto.response.TokenResponseDTO;
-import org.growify.bank.service.strategy.AuthService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.growify.bank.service.AccountService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.*;
+
+import javax.security.auth.login.AccountLockedException;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/v1/auth")
+@RequiredArgsConstructor
 public class AuthController {
-    @Autowired
-    private AuthService authService;
+
+    private final AuthenticationManager authenticationManager;
+    private final AccountService accountService;
 
     @PostMapping("/login")
-    public ResponseEntity<TokenResponseDTO> login(@RequestBody LoginRequestDTO authDto) {
-        return null;
+    public ResponseEntity<TokenResponseDTO> login(@RequestBody LoginRequestDTO authDto) throws AccountLockedException {
+        return accountService.loginAccount(authDto, authenticationManager);
     }
 
     @PostMapping("/register")
-    public ResponseEntity<TokenResponseDTO> register(@RequestBody RegisterRequestDTO registerRequestDTO) {
-        return null;
+    public ResponseEntity<TokenResponseDTO> register(@RequestBody @Valid RegisterRequestDTO registerRequestDTO) {
+        return accountService.registerAccount(registerRequestDTO);
     }
 
-    @PostMapping("/refreshToken")
-    public ResponseEntity<TokenResponseDTO> refreshToken(@RequestBody RefreshTokenDTO refreshTokenDTO) {
-        return null;
+    @PostMapping("/refresh-token")
+    public ResponseEntity<TokenResponseDTO> refreshToken(@RequestBody RefreshTokenRequestDTO refreshTokenRequestDTO) {
+        return accountService.refreshToken(refreshTokenRequestDTO);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        new SecurityContextLogoutHandler().logout(request, response, authentication);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/csrf-token")
+    public CsrfToken csrfToken(HttpServletRequest request) {
+        return (CsrfToken) request.getAttribute(CsrfToken.class.getName());
     }
 }
