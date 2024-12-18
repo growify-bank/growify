a57aedc6f19cedfbbe6c2443fd472ee9684e3ffc
Rafael Silva <ainbot6@gmail.com>
2024-12-17 22:37:39 -0300

    refactor: change record to class

--- src/main/java/org/growify/bank/dto/response/UserResponseDTO.java
+++ src/main/java/org/growify/bank/dto/response/UserResponseDTO.java
@@ -1,4 +1,14 @@
 package org.growify.bank.dto.response;
 
-public record UserResponseDTO(String token, String refreshToken) {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponseDTO {
+    private String id;
+    private String name;
+    private String email;
 }
