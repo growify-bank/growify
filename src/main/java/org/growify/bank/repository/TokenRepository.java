c460d5df4b94aa514862807d9aba8dff4160a135
Rafael Silva <ainbot6@gmail.com>
2024-12-17 22:40:28 -0300

    refactor: change the parameter of the findAllValidTokenByUser method to only receive User class

--- src/main/java/org/growify/bank/repository/TokenRepository.java
+++ src/main/java/org/growify/bank/repository/TokenRepository.java
@@ -5,14 +5,13 @@ import org.growify.bank.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface TokenRepository extends JpaRepository<Token, String> {
-    List<Token> findAllValidTokenByUser(String id);
+    List<Token> findAllValidTokenByUser(User user);
     List<Token> findAllByUserId(String userId);
     Optional<Token> findByTokenValue(String token);
-    List<Token> deleteByUser(User user);
+    void deleteByUser(User user);
 }
