e9987b0f8a9d81f531caf24ed92bb42f2f81afc9
ronaldoss8 <ronaldosssantos1@gmail.com>
2025-02-12 15:08:33 -0300

    feat-implement-deposit #corrected

--- src/main/java/org/growify/bank/controller/DepositController.java
+++ src/main/java/org/growify/bank/controller/DepositController.java
@@ -1,7 +1,13 @@
 package org.growify.bank.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.growify.bank.dto.request.DepositRequest;
+import org.growify.bank.dto.response.DepositResponse;
+import org.growify.bank.service.DepositService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
